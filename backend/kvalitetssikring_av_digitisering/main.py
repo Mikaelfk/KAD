1cb8b6c3c2e9bed86efeca87870074a11bca60a4
Jakob <jakkar@mailbox.org>
2022-02-24 15:14:15 +0100

    Added initial API code

---
+++ backend/kvalitetssikring_av_digitisering/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, Response
+from werkzeug.utils import secure_filename
+from .file_validation import jhove_validation
+import configparser
+import os
+
+
+def start():
+    config = configparser.ConfigParser()
+    config.read("./config")
+
+    app = Flask(__name__)
+    app.config["UPLOAD_FOLDER"] = config["API"]["UploadFolder"]
+    app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 1024  # 1GB
+
+    @app.route("/api/validate", methods=["POST"])
+    def validate():
+        # check if there is a file
+        if "file" not in request.files:
+            print("a")
+            return Response(status=400)
+
+        file = request.files["file"]
+
+        # check if the file has a name
+        if file.filename == "":
+            return Response(status=400)
+
+        # check if file has valid extension
+        file_ext = os.path.splitext(file.filename)[1]
+        if file_ext not in {".jpeg", ".jpg", ".tiff"}:
+            return Response(status=400)
+
+        # lets go upload time!
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+        # check file
+        validation_output = jhove_validation(
+            os.path.join(config["API"]["UploadFolder"], filename),
+            config["JHOVE"]["JhoveInstallPath"],
+        )
+
+        # return result
+        return Response("{isValid:" + str(validation_output[1]) + "}", status=200)
+
+    app.run()
