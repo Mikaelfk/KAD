e34e211cd2dcf4c5ce112397db10f608e43cbfac
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2022-03-03 12:50:50 +0100

    Removed unused import

--- frontend/src/pages/upload-object/UploadObject.jsx
+++ frontend/src/pages/upload-object/UploadObject.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Typography } from '@mui/material';
 import { UploadButton, SubmitButton, CancelButton } from '../../components/Buttons';
 import { Link } from 'react-router-dom';
