febb7d9fe0a337cc31473575780c87c9f53e9762
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2022-03-04 13:03:09 +0100

    Made the parser extract result for each check

--- backend/kvalitetssikring_av_digitisering/tools/os_qm_tool/parser.py
+++ backend/kvalitetssikring_av_digitisering/tools/os_qm_tool/parser.py
@@ -1,12 +1,38 @@
 import os
+from unicodedata import name
+from dataclasses import dataclass, fields
+from warnings import catch_warnings
+
+@dataclass
+class Check:
+    result: bool
+
+@dataclass
+class Results:
+    delta_e: dataclass = Check(None)
+    noise: dataclass = Check(None)
+    oecf: dataclass = Check(None)
+    mtf: dataclass = Check(None)
+    homogeneity: dataclass = Check(None)
+    geometry: dataclass = Check(None)
+
+    @staticmethod
+    def print():
+        for field in fields(Results):
+            try: 
+                print(field.name, getattr(Results, field.name))
+            except: 
+                continue
 
 
 def result_parser(url):
+    data = Results
+
     # Variables
     section_divider = "***********************************************************************"
-    new_section = False
+    section = False
     section_name = ''
-    section_content = ''
+    section_content = []
 
     # Read file
     with open(url) as f:
@@ -18,50 +44,40 @@ def result_parser(url):
         if line.__contains__(section_divider):
             # handle section content
             match section_name.lower():
-                case "delta e": check_delta_e(section_content)
-                case "noise": check_noise(section_content)
-                case "oecf": check_oecf(section_content)
-                case "mtf": check_mtf(section_content)
-                case "homogeneity": check_homogeneity(section_content)
-                case "geometry": check_geometry(section_content)
+                case "delta e": data.delta_e = check_section(section_content)
+                case "noise": data.noise = check_section(section_content)
+                case "oecf": data.oecf = check_section(section_content)
+                case "mtf": data.mtf = check_section(section_content)
+                case "homogeneity": data.homogeneity = check_section(section_content)
+                case "geometry": data.geometry = check_section(section_content)
             # New section
-            new_section = True
-            section_content = ''
+            section = True
+            section_name = ''
+            section_content.clear()
             continue
 
         # set section name
-        if new_section:
-            new_section = False
+        if section_name == '':
             section_name = line.replace(' check', '').strip()
-            continue
 
         # get section content
-        section_content += line
+        if section and line.strip():
+            section_content.append(line)
+    data.print()
 
 
-def check_delta_e(lines):
-    print(lines)
 
-
-def check_noise(lines):
-    print(lines)
-
-
-def check_oecf(lines):
-    print(lines)
-
-
-def check_mtf(lines):
-    print(lines)
-
-
-def check_homogeneity(lines):
-    print(lines)
-
-
-def check_geometry(lines):
-    print(lines)
+def check_section(lines):
+    result = False
+    for line in lines:
+        line = line.lower().strip()
+        if line.startswith("result"):
+            arr = line.split()
+            if arr[1] == 'passed': result = True;
+    
+    return Check(result)
 
 
 # Temp to test out parser
-result_parser(r"C:\Users\Martin Holtmon\Documents\OSQMTOOL\runs\UTT\UTT_protokoll_summary.txt")
+result_parser(
+    r"C:\Users\Martin Holtmon\Documents\OSQMTOOL\runs\GTDevice\GTDevice_protokoll_summary.txt")
