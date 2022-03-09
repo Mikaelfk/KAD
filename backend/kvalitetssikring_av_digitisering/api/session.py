cc1fb9362f6215f13df6ed9d8d7754cd7fe98803
Jakob <jakkar@mailbox.org>
2022-03-09 11:21:28 +0100

    Fixed syntax error in responses

--- backend/kvalitetssikring_av_digitisering/api/session.py
+++ backend/kvalitetssikring_av_digitisering/api/session.py
@@ -16,7 +16,6 @@ session_endpoint = Blueprint("session_endpoint", __name__)
 @session_endpoint.route("/api/session/create", methods=["GET"])
 def create():
     session_id = create_session()
-    update_session_status(id, "created")
 
     return Response(json.dumps({"session_id": session_id}), status=200)
 
@@ -29,29 +28,32 @@ def update():
             session_id = data.get("session_id")
             new_status = data.get("status")
         else:
-            return Response(json.dumps({"error": "invalid json"}, status=400))
+            return Response(json.dumps({"error": "invalid json"}), status=400)
 
-        if check_session_exists(session_id):
-            return Response(json.dumps({"error": "session does not exist"}, status=400))
+        if not check_session_exists(session_id):
+            return Response(json.dumps({"error": "session does not exist"}), status=400)
 
         update_session_status(session_id, new_status)
+        return Response(status=200)
     else:
-        return Response(json.dumps({"error": "invalid json"}, status=400))
+        return Response(json.dumps({"error": "invalid json"}), status=400)
 
 
 @session_endpoint.route("/api/session/createAnalysisFolders", methods=["GET"])
 def create_folders():
     if request.is_json:
-        data = request.get_json
+        data = request.get_json()
         if data is not None:
             session_id = data.get("session_id")
         else:
             return Response(json.dumps({"error": "invalid json"}), status=400)
 
-        if check_session_exists(session_id):
-            return Response(json.dumps({"error": "session does not exist"}, status=400))
+        if not check_session_exists(session_id):
+            return Response(json.dumps({"error": "session does not exist"}), status=400)
 
         create_analysis_folders(session_id)
+
+        return Response(status=200)
 
 
 @session_endpoint.route("/api/session/exists", methods=["GET"])
