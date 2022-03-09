4b18eaff6a28fb9f3caa02ff4fb3086554f7430b
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2022-03-09 13:29:53 +0100

    Removed pytest import and added docstring

--- backend/tests/test_jhove_validation.py
+++ backend/tests/test_jhove_validation.py
@@ -1,4 +1,3 @@
-import pytest
 import os
 import configparser
 
@@ -11,12 +10,18 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def test_jhove_validation_good():
+    """Testing JHOVE validation on good image
+    """
+
     image_good = THIS_DIR + "/test_pictures/test_image.jpg"
     output = jhove_validation(image_good, config["JHOVE"]["JhoveInstallPath"])
     assert output[1] == True
 
 
 def test_jhove_validation_bad():
+    """Testing JHOVE validation on bad image
+    """
+
     image_bad = THIS_DIR + "/test_pictures/test_image-bad.jpg"
     output = jhove_validation(image_bad, config["JHOVE"]["JhoveInstallPath"])
     assert output[1] == False
