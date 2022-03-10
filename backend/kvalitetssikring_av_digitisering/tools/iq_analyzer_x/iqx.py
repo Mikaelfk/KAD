7c1773a0ccf4ac631ab57ff583c1dd2e1363a476
Jakob <jakkar@mailbox.org>
2022-03-10 13:36:20 +0100

    Added initial method for performing analysis with IQX

---
+++ backend/kvalitetssikring_av_digitisering/tools/iq_analyzer_x/iqx.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+
+from kvalitetssikring_av_digitisering.config import Config
+
+
+def runAnalysis(image_file_path: str, specification_level: str):
+
+    if specification_level not in ["A", "B", "C"]:
+        return False
+
+    # static args
+    iqx_executable = os.path.join(
+        Config.config().get(section="IQ ANALYZER X", option="InstallPath"),
+        "iQ-Analyzer-X.exe",
+    )
+    reference = "--reference='-1'"
+    utt = "--utt"
+    exif = "--preferExif"
+    settings_id = "--settingsID=1"
+
+    # variable args
+    image_file = os.path.normpath("{}".format(image_file_path))
+    specification = "--specification='1'".format(1)
+
+    # order of arguments somewhat arbitrary, but chose same as example in manual just in case
+    subprocess.run(
+        [
+            iqx_executable,
+            image_file,
+            settings_id,
+            reference,
+            utt,
+            specification,
+            exif,
+        ],
+    )
+
+    return True
