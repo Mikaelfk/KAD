673b1ecafe77b0465060da764a5b8daec239eaf0
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2022-03-11 15:52:12 +0100

    created a function that fixed invalid xml results from iqx

--- backend/kvalitetssikring_av_digitisering/tools/iq_analyzer_x/parser.py
+++ backend/kvalitetssikring_av_digitisering/tools/iq_analyzer_x/parser.py
@@ -1,6 +1,7 @@
 """Parser for IQ-Analyzer X
 """
 import xml.etree.ElementTree as ET
+import os
 
 
 def result_summary_parser(uri):
@@ -12,10 +13,11 @@ def result_summary_parser(uri):
     Returns:
         dict: Overview results of analysis
     """
+    # fix xml
+    fixed_xml = fix_xml(uri)
 
     # Parse XML to find overview
-    tree = ET.parse(uri)
-    root = tree.getroot()
+    root = ET.fromstring(fixed_xml)
     overview_groups = root.findall('./Group/Image/Curves/UTT/Overview/*')
 
     # Get results
@@ -31,3 +33,23 @@ def result_summary_parser(uri):
         results.update({group.attrib.get('Type'): children_states})
 
     return results
+
+
+def fix_xml(uri):
+    """Replace invalid FXML tag analysis result
+
+    Args:
+        uri (str): path to analysis
+
+    Returns:
+        str: xml result as a string
+    """
+    # Import data
+    with open(uri, 'r', encoding='UTF-8') as file:
+        data = file.read().replace('\n', '')
+    
+    # Fix data
+    data = data.replace('<Maximum delta L>', '<Maximum Type="delta L">')
+    data = data.replace('</Maximum delta L>', '</Maximum>')
+
+    return data
