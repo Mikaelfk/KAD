f9e2bc407ffce0f8ed0cda8055ca15f0b2fe886f
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2022-03-24 13:16:53 +0100

    OS QM-Tool endpoint now analyses uploaded images and saves result to outputs folder

--- backend/kvalitetssikring_av_digitisering/api/oqt_endpoints.py
+++ backend/kvalitetssikring_av_digitisering/api/oqt_endpoints.py
@@ -1,12 +1,41 @@
+import json
 from flask import Blueprint, request
 from flask.wrappers import Response
 
-from kvalitetssikring_av_digitisering.tools.os_qm_tool.oqt import run_analysis
+from kvalitetssikring_av_digitisering.tools.os_qm_tool.oqt import (
+    run_analyses_all_images,
+    run_analysis,
+)
+from kvalitetssikring_av_digitisering.utils.path_helpers import get_session_image_file
+from kvalitetssikring_av_digitisering.utils.session_manager import (
+    create_analysis_folders,
+    create_session,
+)
 
 oqt_endpoint = Blueprint("oqt_endpoint", __name__)
 
 
 @oqt_endpoint.route("/api/analyze/device/oqt", methods=["POST"])
 def analyze():
+    target = request.args.get("target")
 
-    return Response(status=200)
+    match target:
+        case "UTT" | "TE263" | "GTObject" | "GTDevice":
+            print("Target which is used: " + target)
+            files = request.files.getlist("files")
+            session_id = create_session()
+
+            for file in files:
+                file.save(get_session_image_file(session_id, str(file.filename)))
+
+            create_analysis_folders(session_id)
+
+            run_analyses_all_images(session_id, target)
+
+            return Response(json.dumps({"session_id": str(session_id)}), status=200)
+
+        case None | "":
+            return Response(json.dumps({"error:": "no target specified"}), status=400)
+
+        case _:
+            return Response(json.dumps({"error": "invalid target"}), status=400)
--- backend/kvalitetssikring_av_digitisering/tools/os_qm_tool/oqt.py
+++ backend/kvalitetssikring_av_digitisering/tools/os_qm_tool/oqt.py
@@ -1,14 +1,76 @@
+"""Module for running analyses with OS QM-Tool
+
+Contains methods for running multiple or single analyses
+"""
 import os
 import subprocess
 
 from kvalitetssikring_av_digitisering.config import Config
+from kvalitetssikring_av_digitisering.utils.path_helpers import (
+    get_analysis_dir,
+    get_analysis_dir_image_file,
+    get_session_images_dir,
+)
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
+def run_analyses_all_images(session_id: str, target_name: str):
+    """Method for running analyses on all images in a session
+
+    Args:
+        session_id (str): session id of the current session
+        target_name (str): image target which is used for analysis
+    """
+    image_dir = get_session_images_dir(session_id)
+
+    # find name of all image files in session
+    image_files = [
+        f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))
+    ]
+
+    for image_name in image_files:
+        run_iso_analysis(image_name, target_name, session_id)
+
+
+def run_iso_analysis(image_name: str, target_name: str, session_id: str):
+    """Method for running all iso level analyses on a single image
+
+    Args:
+        image_name (str): image to be analyzed
+        target_name (str): image target which is used for analysis
+        session_id (str): session id of the current session
+    """
+    for specification_level in ["A", "B", "C"]:
+        image_path = get_analysis_dir_image_file(
+            session_id, image_name, specification_level
+        )
+
+        run_analysis(
+            image_path,
+            target_name,
+            os.path.join(
+                get_analysis_dir(session_id, image_name, specification_level),
+                "result.txt",
+            ),
+            specification_level,
+        )
+
+
 def run_analysis(
-    image_file: str, target_name: str, output_file: str, specification_level: str
+    image_file_path: str, target_name: str, output_file: str, specification_level: str
 ):
+    """Method for running analysis on a single image with a given specification level
+
+    Args:
+        image_file_path (str): absolute path to the image which should be analyzed
+        target_name (str): image target which is used for analysis
+        output_file (str): path to where the result should be saved
+        specification_level (str): what specification level the analysis should be
+
+    Returns:
+        bool: True if analysis has been run, false otherwis
+    """
 
     if specification_level not in ["A", "B", "C"]:
         return False
@@ -22,12 +84,6 @@ def run_analysis(
         case _:
             return False
 
-    print(parameter_file)
-
-    print(image_file)
-
-    print(output_file)
-
     oqt_executable = os.path.join(
         Config.config().get(section="OS QM-Tool", option="InstallPath"),
         "QMTool.exe",
@@ -37,7 +93,7 @@ def run_analysis(
         subprocess.run(
             [
                 oqt_executable,
-                image_file,
+                image_file_path,
                 parameter_file,
                 output_file,
             ],
