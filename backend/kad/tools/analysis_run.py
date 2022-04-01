aed95f4fea2e6c1247df75f93dee66594def538d
Jakob <jakkar@mailbox.org>
2022-04-01 14:35:56 +0200

    Added new endpoints for running analyses

---
+++ backend/kad/tools/analysis_run.py
@@ -0,0 +1,20 @@
+"""Module for starting ISO 19264 analysis for device or object level targets.
+"""
+
+
+def run_device_analysis(
+    before_target_filename: str,
+    after_target_filename: str,
+    session_id: str,
+    iqes: str,
+    target: str,
+):
+    print("not ready")
+
+
+def run_object_analysis(
+    session_id: str,
+    iges: str,
+    target: str,
+):
+    print("not ready")
