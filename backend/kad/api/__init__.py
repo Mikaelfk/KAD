a3d1a3733719b7b732c958a35f83df942f4d3cf5
Jakob <jakkar@mailbox.org>
2022-04-04 11:44:51 +0200

    Registered new analyze endpoints

--- backend/kad/api/__init__.py
+++ backend/kad/api/__init__.py
@@ -2,9 +2,10 @@
 
 """
 
+from .analyze_endpoints import analyze_endpoint
+from .download_endpoints import download_endpoint
 from .iqx_endpoints import iqx_endpoint
+from .oqt_endpoints import oqt_endpoint
 from .results_endpoints import results_endpoint
 from .session_endpoints import session_endpoint
 from .validate_endpoints import validate_endpoint
-from .oqt_endpoints import oqt_endpoint
-from .download_endpoints import download_endpoint
